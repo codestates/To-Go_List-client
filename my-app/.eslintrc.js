422105fb272ac3f80e38f7f4e69babe4441f3cfd
GriGni <amistadlee10@gmail.com>
2020-11-11 11:12:14 +0900

    setting the proj

--- my-app/.eslintrc.js
+++ my-app/.eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
     "env": {
         "browser": true,
-        "commonjs": true,
         "es2021": true
     },
     "extends": [
@@ -12,7 +11,8 @@ module.exports = {
         "ecmaFeatures": {
             "jsx": true
         },
-        "ecmaVersion": 12
+        "ecmaVersion": 12,
+        "sourceType": "module"
     },
     "plugins": [
         "react"
