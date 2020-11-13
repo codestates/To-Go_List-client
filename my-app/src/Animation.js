8dd71e6bb06f2fd45c6c2ca4ac454b34f76f0399
GriGni <amistadlee10@gmail.com>
2020-11-13 15:47:08 +0900

    [client]. making the layout of main_page

--- my-app/src/Animation.js
+++ my-app/src/Animation.js
@@ -1,11 +1,10 @@
 /* eslint-disable */
-window.onload =function(){
-  (()=>{
+(()=>{
+      console.log("pass")
     let yOffset = 0;
     let prevScrollHeight = 0;
     let currentScene = 0;
-    console.log(yOffset, currentScene)
-  
+
     const sceneInfo = [
       {
         type: 'normal',
@@ -20,8 +19,31 @@ window.onload =function(){
         heightNum:3,
         scrollHeight:0,
         objs:{
-          container: document.querySelector("#scroll_section_1")
-        }
+          container: document.querySelector("#scroll_section_1"),
+          messageA: document.querySelector('#scroll_section_0 .main_message.a'),
+          messageB: document.querySelector('#scroll_section_0 .main_message.b'),
+          messageC: document.querySelector('#scroll_section_0 .main_message.c'),
+          messageD: document.querySelector('#scroll_section_0 .main_message.d'),
+        },
+        values:{
+          messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
+          messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
+          messageC_opacity_in: [0, 1, { start: 0.5, end: 0.6 }],
+          messageD_opacity_in: [0, 1, { start: 0.7, end: 0.8 }],
+          messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2 }],
+          messageB_translateY_in: [20, 0, { start: 0.3, end: 0.4 }],
+          messageC_translateY_in: [20, 0, { start: 0.5, end: 0.6 }],
+          messageD_translateY_in: [20, 0, { start: 0.7, end: 0.8 }],
+          messageA_opacity_out: [1, 0, { start: 0.25, end: 0.3 }],
+          messageB_opacity_out: [1, 0, { start: 0.45, end: 0.5 }],
+          messageC_opacity_out: [1, 0, { start: 0.65, end: 0.7 }],
+          messageD_opacity_out: [1, 0, { start: 0.85, end: 0.9 }],
+          messageA_translateY_out: [0, -20, { start: 0.25, end: 0.3 }],
+          messageB_translateY_out: [0, -20, { start: 0.45, end: 0.5 }],
+          messageC_translateY_out: [0, -20, { start: 0.65, end: 0.7 }],
+          messageD_translateY_out: [0, -20, { start: 0.85, end: 0.9 }]
+          
+      }
       },
       {
         type: 'normal',
@@ -40,32 +62,61 @@ window.onload =function(){
         }
       },
     ]
-    //1
+  //   //1
     function setLayout(){
       for(let i = 0; i < sceneInfo.length; i++){
           sceneInfo[i].scrollHeight = sceneInfo[i].heightNum* window.innerHeight;
-          sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}`;
-        }
-        console.log(sceneInfo)
-  
+          sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+      }
     }
-  
+//     function setLayout(){
+//       //각 스크롤 섹션의 높이 세팅
+//       for(let i =0; i < sceneInfo.length; i++){
+//         if(sceneInfo[i].type === 'sticky'){
+//           sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight // 브라우저 창의 높이
+//           sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`
+//         }else if(sceneInfo[i].type === 'normal'){
+//          sceneInfo[i].scrollHeight = sceneInfo[i].objs.container.offsetHeight;
+//         }
+//         sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`
+//        }
+//        // console.log(sceneInfo)
+//        // 현재 스크롤에 맞춰서 currentscene을 자동으로 맞추는 기능
+//        let totalScrollHeight = 0;
+//        yOffset = window.pageYOffset
+//        for(let i = 0; i < sceneInfo.length; i++){
+//            totalScrollHeight += sceneInfo[i].scrollHeight;
+//            if(totalScrollHeight >= yOffset){
+//                currentScene = i;
+//                // 반복문 탈출!
+//                break;
+//            }
+//        }
+//        document.body.setAttribute('id', `show_scene_${currentScene}`)
+//    }
+
     function scrollLoop(){
-      for(let i = 0; i < sceneInfo.length; i++){
+      prevScrollHeight = 0;
+      for(let i = 0; i < currentScene; i++){
         prevScrollHeight = prevScrollHeight + sceneInfo[i].scrollHeight;
       }
-      console.log(prevScrollHeight)
+      if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight){
+        currentScene++;
+      }else if(yOffset < prevScrollHeight){
+        if(currentScene === 0) return
+        currentScene--;
+      }
+      document.body.setAttribute('id', `show_scene_${currentScene}`)
     }
-    window.addEventListener("scroll", () => {
-      yOffset = window.pageYOffset
+    
+
+  
+  window.addEventListener('scroll', () =>{
+      yOffset = window.pageYOffset;
+      console.log(yOffset)
       scrollLoop();
-    })
+  })
     window.addEventListener("resize", setLayout);
-    // function scrollLoop(){
-  
-    // }
-  
+    // window.addEventListener("load", setLayout);
     setLayout();
   })();  
-}
-
--- my-app/src/routes/Mainpage.js
+++ my-app/src/routes/Mainpage.js
@@ -1,21 +1,62 @@
+/* eslint-disable */
 import React from "react";
 import './Mainpage.css';
 import { Link } from "react-router-dom"
-import "../Animation.js"
+import axios from "axios"
+import { UNSPLASH_API_KEY } from "../config/unsplash"
+// import Animation from "../Animation.js"
 
 class Mainpage extends React.Component{
-  
-  componentDidMount(){
-    const script = document.createElement("script");
-    script.src = "../Animation.js";
-    script.type="text/jsx"
-    script.async = true;
-    document.body.appendChild(script)
-    console.log("pass didMount")
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoading: true,
+      photos:[],
+      searh:"",
+      clientId: UNSPLASH_API_KEY,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  getPhots = async () => {
+    const photos = await axios.get();
+  }
+  componentDidMount(){
+    // const script = document.createElement("script");
+    // script.src = "../Animation.js";
+    // script.type="text/jsx"
+    // script.async = true;
+    // document.body.appendChild(script)
+    // window.addEventListener('load', Animation)
+    
+    // setTimeout(() => {
+    //   this.setState({isLoading: false})
+    // }, 5000)
+  }
+
   
+
+  handleChange = (e) => {
+    this.setState({
+      searh: e.target.value,
+    })
+  }
+ 
+  handleSubmit = (e) => {
+    const clientId= this.state.clientId
+    const searhResult = this.state.searh
+    const url = `https://api.unsplash.com/search/photos?page=1&query=${searhResult}&client_id=${clientId}` 
+    console.log(url)
+    axios.get(url).then((res) => {
+      this.setState({
+        photos: res.data.results
+      })
+    })
+ }
+
   render(){
-    console.log("pass render")
+    const {isLoading} = this.state;
     return(
       <div className="container">
 	      <nav className="local_nav">
@@ -27,57 +68,72 @@ class Mainpage extends React.Component{
 	      </nav> 
 		    <section className="scroll_section" id="scroll_section_0">
 		      <img className="logo"src="http://penzim.synology.me/image/To-Go_List-logo-black.png"></img>
+          <h1 className ="main_message">To-Go_List와 함께<br></br> 체계적으로 가고 싶은 곳을 정리해요</h1>
 		    </section>
 		    <section className="scroll_section" id="scroll_section_1">
-              <div className = "sticky_elem main_message a">
-                <p>
-
-                  To-Go_List와 함께<br></br> 체계적으로 가고 싶은 곳을 정리해요
-                </p>
-              </div>
-              <div className = "sticky_elem main_message b">
-                <p>
-
-                  인스타그램에서 봤던 핫플레이스를 <br></br>나만의 리스트에 담아
-                </p>
-              </div>
-              <div className = "sticky_elem main_message c">
-                <p>
-
-                  인스타그램에서 봤던 핫플레이스를<br></br> 나만의 리스트에 담아
-                </p>
-              </div>
-              <div className = "sticky_elem main_message d">
-                <p>            
-                  인스타그램에서 봤던 핫플레이스를<br></br> 나만의 리스트에 담아
-                </p>
-              </div>
+            {this.state.photos.map((photo) => {
+              <img src = {photo.urls.small} />
+            })}
+            <div className="photos">
+            <div className ="photo">1</div>
+            <div className ="photo">2</div>
+            <div className ="photo">3</div>
+            <div className ="photo">4</div>
+            <div className ="photo">5</div>
+            <div className ="photo">6</div>
+          </div>
+          <div className ="search_photos">
+            <input className = "input" type = "text" placeholder = "search for here" onChange = {this.handleChange}/>
+            <button className = "btn" onClick = {this.handleSubmit}>Search</button>
+          </div>
+          <p>
+            다른 사람이 가서 찍은 사진을 미리 보고 가고 싶은 공간으로 등록하기~
+          </p>
         </section>
 		    <section className="scroll_section" id="scroll_section_2">
           {/*  기능 구현 동영상 또는 한국 홍보 영상 등등 */}
-          gif 틀 구성 이랑 내용저장 
-          <h1>  1.기능설명 : 검색 
+          {/* <h1>  1.기능설명 : 검색 
             기능은 태그를 이용해서 저장했던 장소를 찾아드립니다. <br></br>
-                2. gif 재생 - </h1>
+          gif 틀 구성 이랑 내용저장 
+                2. gif 재생 - </h1> */}
+          <div className = "gif_part">
+            gif 파일
+          </div>
+          <div className = "gif_description">
+            <p> 기능 설명<br></br> 
+              1. 저장했던 장소는 태그했던 내용을 바탕으로 검색하면 됩니다.<br></br>
+              2. 저장한 데이터를 마이페이지에서 쉽게 확인 가능합니다<br></br>
+              3. 우선순위를 설정해서 가고 싶은 장소의 순위를 정할 수 있습니다.
+            </p>
+          </div>
 
-                
         </section>
 		    <section className="scroll_section" id="scroll_section_3">
-            <p>
-              1. 한국 관광지 이미지 넣기, 인기있는 가게도 괜춘!, 한국 영상
+            
+            <p className="mid-message">
+            <strong>보통 스크롤 영역</strong>
+          1. 한국 관광지 이미지 넣기, 인기있는 가게도 괜춘!, 한국 영상
               2. 개인적으로 평점 순으로 지역 한정해서 높은 순서로 맛집리스트를 뿌려주면 좋을 것 같습니다. 가능할까요??
                그러면 여기에 컴포넌트를 넣을게요~~ 
               3. fake 데이터로 일단 구성.  맛집, 관광지 공간
               4. 틀... 
               5. 한국 관광지 이미지 넣기, 인기있는 가게도 괜춘!, 한국 영상
-            </p>
+             </p>
           </section>
           <footer className="footer">
-            <p>
-              여긴 푸터다<br></br>
-              만든사람: 시간과 실력과 돈이 있었더라면...<br></br>
-              사용한 기술 레퍼런스: react-router-dom, node.js, express, mysql, html, css, javascript etc
-            </p>
+            <div className = "info">
+              <div className ="developer_info">
+                <p>
+                  <img></img>
+                  만든사람: 시간과 실력과 돈이 있었더라면
+                </p>
+              </div>
+            <div className ="technical_reference">
+              <p>
+              사용한 기술 레퍼런스: react, node.js, mysql, html, css, javascript
+              </p>
+            </div>
+            </div>
           </footer>
 	    </div>
       )
