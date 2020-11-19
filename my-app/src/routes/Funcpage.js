e636ee05de3ff92fbf90f375a89010fee46a7ede
배채겸 <bae-chaegyeom@baechaegyeoms-MacBook-Pro.local>
2020-11-19 22:44:21 +0900

    Implement map function

--- my-app/src/routes/Funcpage.js
+++ my-app/src/routes/Funcpage.js
@@ -4,20 +4,55 @@ import React from "react";
 import Nav from "../components/Func_page/Nav";
 import ToGoList from "../components/Func_page/ToGoList";
 import Post from "../components/Func_page/Post";
+import axios from "axios"
 
 import './Funcpage.css'
+
+axios.defaults.withCredentials = true;
 class Funcpage extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
         }
     }
+
+    componentDidMount() {
+        console.log("기능페이지 didmount중입니다")
+        
+        
+        axios({
+            method: 'get',
+            url: 'https://togolist-server.ml/post',
+        },{withCredentials :true}
+        ).then((res) => {
+                this.setState({
+                    userPost:res.data
+                })
+            console.log("기능페이지 포스트 받아오니?",res)
+            })
+    }
+
+
+    createPost = () => {
+        axios({
+            method: 'get',
+            url: 'https://togolist-server.ml/post',
+        },{withCredentials :true}
+        ).then((res) => {
+                this.setState({
+                    userPost:res.data
+                })
+            console.log("생성 된거니?",res)
+            })
+    }
+
+
     render() {
         return (
             <div className="start_page">
                 <Nav></Nav>
-                <Post></Post>
-                <ToGoList></ToGoList>
+                <Post createpost={this.createPost }></Post>
+                <ToGoList ></ToGoList>
             </div>
         )
     }
