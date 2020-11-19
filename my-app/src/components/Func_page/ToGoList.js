206bcdc5d596cca6d4a8d90084e4abc5a26181a9
parkjikuk <paxkk@naver.com>
2020-11-19 18:04:48 +0900

    func page list

--- my-app/src/components/Func_page/ToGoList.js
+++ my-app/src/components/Func_page/ToGoList.js
@@ -1,13 +1,34 @@
+/* eslint-disable */
+
 import React from "react";
+import axios from "axios"
 
 
 class ToGoList extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            content: "",
+            location: "",
+            mapimgpath: "",
+            tag: "",
+            info: ""
         }
     }
+
+    componentDidMount() {
+        axios({
+            method: "get",
+            url: "https://togolist.com/post"
+        })
+            .then((res) => {
+                console.log(res)
+                this.setState({
+                    content: res.data.content
+                })
+            })
+    }
+
 
     render() {
         return (
@@ -16,8 +37,8 @@ class ToGoList extends React.Component {
                     <h1>To Go List</h1>
                     <div className="start_content_frame">
                         <div className="mypage_mycontent_map"></div>
-                        <div className="mypage_mycontent_content">내용이 들어갈 겁니다!</div>
-                        <div className="mypage_mycontent_tag">태그태그</div>
+                        <div className="mypage_mycontent_content">{this.props.content}</div>
+                        <div className="mypage_mycontent_tag">{this.props.tag}</div>
                         <div>
                             <button className="edit_post">수정</button>
                             <button className="remove_post">삭제</button>
